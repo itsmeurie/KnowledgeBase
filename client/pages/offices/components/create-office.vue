d4673d32437846e581f4c1337efc1190a75b3e5e
itsmeurie <132588774+itsmeurie@users.noreply.github.com>
2025-03-20 13:38:09 +0800

    requiredniurie

--- client/pages/offices/components/create-office.vue
+++ client/pages/offices/components/create-office.vue
@@ -12,13 +12,6 @@ const props = defineProps({
 
 const emit = defineEmits(['update:show'])
 
-const validate = (state: any): FormError[] => {
-  const errors = []
-  if (!state.name) errors.push({ path: 'name', message: 'Required' })
-  if (!state.code) errors.push({ path: 'code', message: 'Required' })
-  return errors
-}
-
 const schema = z.object({
   name: z.string(),
   code: z.string().min(3, 'Must be at least 3 characters')
@@ -31,15 +24,24 @@ const state = reactive({
   code: undefined
 })
 
+const validate = (state: any): FormError[] => {
+  const errors = []
+  if (!state.name) errors.push({ path: 'name', message: 'Required' })
+  if (!state.code) errors.push({ path: 'code', message: 'Required' })
+  return errors
+}
+
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   // Do something with data
-  console.log(event.data)
   $api.post('/offices', event.data)
         .then((response) => {
             if(response.status == 200)
                 emit('update:show', false)
         })
+  console.log(event.data)
+ 
 }
+
 
 function toggleModal(state : boolean | undefined){
     emit("update:show", state)
@@ -49,7 +51,7 @@ function toggleModal(state : boolean | undefined){
 
 
 <template>
-    <TSlideover :modelValue="show" @update:modelValue="toggleModal">
+    <TSlideover :modelValue="show" @update:modelValue="toggleModal" >
         <div class="p-4 flex-1">
             <TButton
             color="gray"
@@ -63,15 +65,16 @@ function toggleModal(state : boolean | undefined){
             />
                 <h2 class="text-xl">Create new Office </h2>
                 <p class="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo officia culpa eveniet voluptas atque mollitia corrupti magnam vel possimus labore. Quaerat animi eaque officiis nemo impedit natus cum debitis minima?</p>
-                <TForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-                    <TFormGroup label="Office Name" name="office_name">
+                <TForm :validate="validate" :state="state" class="space-y-4" @submit="onSubmit" :schema="schema" >
+                    <TFormGroup label="Office Name" name="name">
                         <TInput v-model="state.name" />
                     </TFormGroup>
 
-                    <TFormGroup label="Office Code" name="office_code">
+                    <TFormGroup label="Office Code" name="code">
                         <TInput v-model="state.code" />
                     </TFormGroup>
 
+                    <div></div>
                     <TButton type="submit">
                         Submit
                     </TButton>
