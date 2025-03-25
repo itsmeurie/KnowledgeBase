50912deaf097c989c90b76d15c1c4e738643a179
Momoring <Joyishere18@gmail.com>
2025-03-25 13:57:16 +0800

    update index.vue
    Code ni carlo kasi nagloko ang laptop

--- client/pages/offices/index.vue
+++ client/pages/offices/index.vue
@@ -14,6 +14,8 @@ const search_term = ref('');
 const is_creating = ref(false);
 const is_updating = ref(false);
 
+
+
 function fetchOfficeList(name: string = "", code: string = "") {
     $api.get('/offices', { params: { name, code } })
         .then((response) => {
@@ -36,33 +38,35 @@ watchDebounced(search_term, (value) => {
 onMounted(() => {
     fetchOfficeList();
 });
-</script>
 
+</script>
 <template>
     <div class="flex flex-col items-center h-full p-4">
         <div class="flex flex-col gap-4 max-w-7xl w-full">
-            <div class="flex flex-col gap-2">
+            <div class="flex flex-col gap-3">
                 <div class="flex justify-between items-center">
-                    <h5 class="text-xl">Office Documentations</h5>
+                    <h5 class="text-2xl font-bold">Office Documentations</h5>
                     <div class="flex justify-between gap-4">
                         <TTooltip @click="toggleCreate(true)" text="Add New System">
                             <TButton icon="tabler:plus" size="sm" />
                         </TTooltip>
                     </div>
                 </div>
-                <p class="text-sm max-w-xl">
+                <p class="text-l max-w-xl">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Nemo earum, quibusdam saepe obcaecati repellat repudiandae officiis, ea exercitationem beatae nihil praesentium, magnam dolores dolorum veritatis explicabo. Aperiam corporis sunt dolorum!
                 </p>
             </div>
             <TInput v-model="search_term" type="text" placeholder="Search for offices" />
-            <div class="grid grid-cols-1 lg:grid-cols-6 justify-center">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 justify-center">
                 <div v-for="office in offices" 
                      :key="office.id" 
-                     class="flex justify-center py-8 border cursor-pointer hover:text-blue-500"
+                     class="flex items-center py-8 border cursor-pointer hover:text-blue-500 gap-4 p-4 text-xl font-bold rounded-lg"
                      @click="goToArticlePage">
-                    {{ office.name }}
-                </div>
+                    
+                    <img class="h-20 w-20" src="~/assets/image/default_seal.png" alt="Office Logo" >
+                    <span>{{ office.name }}</span>
+                </div>  
             </div>
         </div>
         <CreateOffice :show="is_creating" @update:show="toggleCreate"></CreateOffice>
