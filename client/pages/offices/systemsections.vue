0623bdb1c1ec990032d01a0d9f133193870ddf3b
JCAventura <johncarloaventura17@gmail.com>
2025-03-31 13:56:38 +0800

    header
    put an logo sa Header

--- client/pages/offices/systemsections.vue
+++ client/pages/offices/systemsections.vue
@@ -7,6 +7,13 @@ const router = useRouter();
 
 const office = ref<Office>();
 const officeSection = ref<Section[]>([])
+const aboutSection = ref<HTMLElement | null>(null);
+
+const scrollToAbout = () => {
+  if (aboutSection.value) {
+    aboutSection.value.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+};
 
 function fetchOffice() {
     $api.get('/offices/', { params: { code: route.params.slug } })
@@ -24,7 +31,7 @@ onMounted(() => {
     fetchOfficeSectionList();
 });
 
-const links = [
+const linksNav = [
   { label: 'Home', to: '/playground' },
   { label: 'Docs', to: '/systemsections' },
   { label: 'About', to: '/playground#about-documentation' }
@@ -50,27 +57,35 @@ const goToArticlePage = ()=>{
 
 <template> 
     <!-- Header -->
-  <div class="flex flex-wrap items-center justify-between px-4 sm:grid sm:grid-cols-8 gap-4">
+  <div class="flex flex-wrap items-center justify-between px-4  gap-4 m-1">
         <!-- Logo -->
-        <div class="flex items-center min-w-60 ">
-          <p class="text-2xl sm:text-3xl font-extrabold text-black font-Inter">Knowledge Base</p>
+        <div class="flex items-center">
+            <img class="h-20 w-20 object-contain mt-6" src="@/assets/image/final-logo.png"/>
+              <p class="text-2xl sm:text-3xl font-extrabold text-black font-Inter">Knowledge Base</p>
         </div>
 
+
         <!-- Spacing divs for layout balance (hidden on small screens) -->
-        <div class="hidden sm:block col-span-1 h-[100px]"></div>
-        <div class="hidden sm:block col-span-1 h-[100px]"></div>
-        <div class="hidden sm:block col-span-1 h-[100px]"></div>
-        <div class="hidden sm:block col-span-1 h-[100px]"></div>
+            <div class="hidden sm:block col-span-1 h-6.25"></div>
+            <div class="hidden sm:block col-span-1 h-6.25"></div>
+            <div class="hidden sm:block col-span-1 h-6.25"></div>
+            <div class="hidden sm:block col-span-1 h-6.25"></div>
 
         <!-- Navigation -->
         <div class="w-full sm:w-auto col-span-3 flex justify-center">
-          <THorizontalNavigation :links="links">
-            <template #default="{ link }">
-                <span class="text-lg mx-4 sm:mx-8 text-black font-Inter font-bold group-hover:text-primary relative">{{ link.label }}</span>
-            </template>
-          </THorizontalNavigation>
+          <THorizontalNavigation :links="linksNav"> <!-- Inayos-->
+                <template #default="{ link }">
+                  <a 
+                    :href="link.to" 
+                    @click.prevent="link.label === 'About' ? scrollToAbout() : null" 
+                    class="text-lg mx-4 sm:mx-8 text-black font-Inter font-bold group-hover:text-primary relative"
+                  >
+                    {{ link.label }}
+                  </a>
+                </template>
+            </THorizontalNavigation>
+            </div>
         </div>
-  </div>
   <div  class="flex flex-col items-center h-full p-4">
     <div class="flex flex-col gap-2 max-w-7xl w-full">
       <div class="bg-cover bg-center bg-no-repeat rounded-md shadow-lg" style="background-image: url('https://static.vecteezy.com/system/resources/previews/009/302/805/non_2x/silhouette-landscape-with-fog-forest-pine-trees-mountains-illustration-of-national-park-view-mist-black-and-white-good-for-wallpaper-background-banner-cover-poster-free-vector.jpg');">
